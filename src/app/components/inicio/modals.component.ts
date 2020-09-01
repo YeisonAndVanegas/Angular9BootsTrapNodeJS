f8ac228b9200ed71752eb574d94afaf78699ad54
Yeison Vanegas <yeisonand.oyola@misena.edu.co>
2020-09-01 11:46:51 -0500

    Obtener Tecnologias

--- src/app/components/inicio/modals.component.ts
+++ src/app/components/inicio/modals.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/service/modal.service';
+import { TecnologiaSobreMiService } from 'src/app/service/tecnologia-sobre-mi.service';
 
 declare let $:any;
 
@@ -11,9 +12,28 @@ declare let $:any;
 })
 export class ModalsComponent implements OnInit {
 
-  constructor(public modalService: ModalService) { }
+  tecnologiasDestacadas: string[] = [];
+
+  tec1: string[] = [];
+  tec2: string[] = [];
+  tec3: string[] = [];
+
+  constructor(
+    public modalService: ModalService,
+    private tecSobre: TecnologiaSobreMiService
+  ) { }
 
   ngOnInit(): void {
+    this.tecSobre.getTecnologia()
+    .subscribe((res: any) => {
+      this.tecnologiasDestacadas.push(...res.tecnologias);
+      this.tec1 = this.tecnologiasDestacadas.slice(0,3);
+      this.tec2 = this.tecnologiasDestacadas.slice(3,6);
+      this.tec3 = this.tecnologiasDestacadas.slice(6,9);
+      console.log(this.tec1);
+      console.log(this.tec2);
+      console.log(this.tec3);
+    });
   }
 
   pagina1(){
